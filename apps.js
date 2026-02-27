f1a486a8078b4f27c45c372f5e61f12cf4759556
ISABELLE DOS SANTOS DE ABREU <25132976@CFP127ALUNO.LOCAL>
2026-02-27 16:55:11 -0300

    criando funções

--- apps.js
+++ apps.js
@@ -1,3 +1,17 @@
 'use strict'
 
 const homeContainer = document.getElementById('home');
+const botaoDs = document.querySelector('.curso-ds');
+
+async function buscarCursos(curso) {
+    const url = `https://lion-school-backend.onrender.com/alunos?curso=${curso}`
+    const response = await fetch (url)
+    const cursos = await response.json()
+
+    return cursos
+    
+}
+
+function criarCard(){
+
+}
